bb0610c51646b9b611b6882ab28a22d0a7ff6e9f
TylerSchaefer4 <93676493+TylerSchaefer4@users.noreply.github.com>
2023-06-22 14:52:53 -0400

    beginning to implement homescreen

--- BudgetBuddy/BudgetBuddy/HomeScreen/ViewController.swift
+++ BudgetBuddy/BudgetBuddy/HomeScreen/ViewController.swift
@@ -8,17 +8,29 @@
 import UIKit
 
 class ViewController: UIViewController {
+    let homeScreen = HomeScreenView()
 
+    
+    override func loadView() {
+        view = homeScreen
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        title = "Financial Summary"
         
         let profileButton = UIBarButtonItem(image: UIImage(systemName: "person.fill"), style: .plain, target: self, action: #selector(onProfileButtonTapped))
         
         navigationItem.rightBarButtonItem = profileButton
+        
+        
     }
     
     @objc func onProfileButtonTapped() {
-        
+        let showProfileController = ShowProfileController()
+        //showProfileController.currentUser =
+        navigationController?.pushViewController(showProfileController, animated: true)
     }
 
 
