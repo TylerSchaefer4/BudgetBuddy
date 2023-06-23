c5641a9611398efa3a669446e34717da5ba08d13
TylerSchaefer4 <93676493+TylerSchaefer4@users.noreply.github.com>
2023-06-23 12:56:16 -0400

    attempting to fix main branch

---
+++ BudgetBuddy/BudgetBuddy/DataModels/Contact.swift
@@ -0,0 +1,23 @@
+//
+//  Contact.swift
+//  BudgetBuddy
+//
+//  Created by Tyler Schaefer on 6/22/23.
+//
+
+import Foundation
+import FirebaseFirestoreSwift
+
+struct Contact: Codable {
+    @DocumentID var id: String?
+    var name: String
+    var email: String
+    var photoURL: String?
+
+    
+    init(name: String, email: String, photoURL: String?) {
+        self.name = name
+        self.email = email
+        self.photoURL = photoURL
+    }
+}
