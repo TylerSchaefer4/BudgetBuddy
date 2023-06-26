e4c78ba63107b49a03511a7a36da7afc722fd968
TylerSchaefer4 <93676493+TylerSchaefer4@users.noreply.github.com>
2023-06-25 20:16:41 -0400

    Finished add transactions

--- BudgetBuddy/BudgetBuddy/AddTransaction/AddTransactionViewController.swift
+++ BudgetBuddy/BudgetBuddy/AddTransaction/AddTransactionViewController.swift
@@ -28,8 +28,40 @@ class AddTransactionViewController: UIViewController {
         //self.getUserInformation()
     }
     
-    @objc func onAddButtonTapped(){
+    @objc func onAddButtonTapped() {
+        guard let amountString = addTransactionView.textFieldAmount.text,
+              let amount = Double(amountString),
+              amount > 0 else {
+            showErrorAlert("Invalid amount")
+            return
+        }
+
+        let type = addTransactionView.selectedPhoneNumberType
+        let nameOfPlace = addTransactionView.textFieldNameOfPlace.text ?? ""
+        let description = addTransactionView.textFieldDescription.text ?? ""
+        let location = addTransactionView.textFieldLocation.text ?? ""
+        let timestamp = Timestamp(date: Date())
         
+        let transaction = Transaction(type: type, amount: amount, nameOfPlace: nameOfPlace, description: description, location: location, timestamp: timestamp)
+
+        saveTransactionToFirebase(transaction)
     }
+
+    func saveTransactionToFirebase(_ transaction: Transaction) {
+        guard let user = Auth.auth().currentUser else {
+            showErrorAlert("No user is signed in.")
+            return
+        }
+        
+        let db = Firestore.firestore()
+        let transactionRef = db.collection("users").document(user.uid).collection("transactions").document()
+        
+        do {
+            try transactionRef.setData(from: transaction)
+        } catch let error {
+            showErrorAlert("Error writing transaction to Firestore: \(error)")
+        }
+    }
+
 
 }
