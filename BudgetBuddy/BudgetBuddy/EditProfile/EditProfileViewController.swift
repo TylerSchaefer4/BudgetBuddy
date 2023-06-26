72a807c2045ee5e4737dc6c882c6bfbff38a5024
muneerlalji <muneerlalji@gmail.com>
2023-06-26 15:26:25 -0400

    register bug fixes as well as profile bug fixes

--- BudgetBuddy/BudgetBuddy/EditProfile/EditProfileViewController.swift
+++ BudgetBuddy/BudgetBuddy/EditProfile/EditProfileViewController.swift
@@ -18,7 +18,7 @@ class EditProfileViewController: UIViewController {
     
     var currentUser:FirebaseAuth.User!
     
-    var profileImage:UIImage?
+    //var profileImage:UIImage?
     
     let storage = Storage.storage()
     
@@ -35,13 +35,30 @@ class EditProfileViewController: UIViewController {
 
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .save, target: self, action: #selector(onSaveButtonTapped))
         
-        editProfileScreen.buttonTakePhoto.menu = getMenuImagePicker()
+        //editProfileScreen.buttonTakePhoto.menu = getMenuImagePicker()
     }
     
     @objc func onSaveButtonTapped() {
-        if let name = editProfileScreen.nameTextField.text, let username = editProfileScreen.usernameTextField.text, let email = editProfileScreen.emailTextField.text {
-            if self.profileImage != nil {
-                self.uploadToStorage(name: name, email: email, image: self.profileImage)
+        if let name = editProfileScreen.nameTextField.text, let email = editProfileScreen.emailTextField.text {
+            //            if self.profileImage != nil {
+            //                self.uploadToStorage(name: name, email: email, image: self.profileImage)
+            //            }
+            do {
+                let user = self.database.collection("users").document(self.currentUser.email!).getDocument {(document, error) in
+                    if let document = document, document.exists {
+                            do {
+                                let data = try document.data(as: User.self)
+                                self.modifyFirebaseProfile(name: name, email: email, oldUserInfo: data)
+                            } catch {
+                                print("decoding error")
+                            }
+                        } else {
+                            print("user not found")
+                        }
+                }
+            }
+            catch {
+                print("error")
             }
         }
         else {
@@ -50,60 +67,61 @@ class EditProfileViewController: UIViewController {
         }
     }
     
-    func uploadToStorage(name:String, email: String, image: UIImage?) {
-        var profilePhotoURL:URL?
-                
-                //MARK: Upload the profile photo if there is any...
-        if let picture = image{
-            if let jpegData = picture.jpegData(compressionQuality: 80){
-                let storageRef = storage.reference()
-                let imagesRepo = storageRef.child("imagesUsers")
-                let imageRef = imagesRepo.child("\(NSUUID().uuidString).jpg")
-                        
-                let uploadTask = imageRef.putData(jpegData, completion: {(metadata, error) in
-                    if error == nil{
-                        imageRef.downloadURL(completion: {(url, error) in
-                            if error == nil{
-                                profilePhotoURL = url
-                                self.modifyFirebaseProfile(name: name, email: email, photoURL: profilePhotoURL)
-                            }
-                        })
-                    }
-                })
-            }
-        }else{
-            modifyFirebaseProfile(name: name, email: email, photoURL: profilePhotoURL)
-        }
-    }
+//    func uploadToStorage(name:String, email: String, image: UIImage?) {
+//        var profilePhotoURL:URL?
+//
+//                //MARK: Upload the profile photo if there is any...
+//        if let picture = image{
+//            if let jpegData = picture.jpegData(compressionQuality: 80){
+//                let storageRef = storage.reference()
+//                let imagesRepo = storageRef.child("imagesUsers")
+//                let imageRef = imagesRepo.child("\(NSUUID().uuidString).jpg")
+//
+//                let uploadTask = imageRef.putData(jpegData, completion: {(metadata, error) in
+//                    if error == nil{
+//                        imageRef.downloadURL(completion: {(url, error) in
+//                            if error == nil{
+//                                profilePhotoURL = url
+//                                self.modifyFirebaseProfile(name: name, email: email, photoURL: profilePhotoURL)
+//                            }
+//                        })
+//                    }
+//                })
+//            }
+//        }else{
+//            modifyFirebaseProfile(name: name, email: email, photoURL: profilePhotoURL)
+//        }
+//    }
     
-    func modifyFirebaseProfile(name: String, email: String, photoURL: URL?){
+    func modifyFirebaseProfile(name: String, email: String, oldUserInfo: User){
         let changeRequest = Auth.auth().currentUser?.createProfileChangeRequest()
         changeRequest?.displayName = name
-        changeRequest?.photoURL = photoURL
+        //changeRequest?.photoURL = photoURL
     
-        print("\(photoURL)")
         changeRequest?.commitChanges(completion: {(error) in
             if error != nil{
                 print("Error occured: \(String(describing: error))")
                 self.showErrorAlert("error")
             }else{
-                Auth.auth().currentUser?.updateEmail(to: email) {error in
+                Auth.auth().currentUser?.updateEmail(to: email.lowercased()) {error in
                     if let error = error {
                         print("failed to update email")
                         self.showErrorAlert("Failed to update email")
                     } else {
-                        self.updateFirestore(name: name, email: email, photoURL: photoURL)
+                        self.updateFirestore(name: name, email: email, user: oldUserInfo)
                     }
                 }
             }
         })
     }
     
-    func updateFirestore(name: String, email: String, photoURL: URL?) {
-        database.collection("users").document(email).setData([
+    func updateFirestore(name: String, email: String, user:User) {
+        database.collection("users").document(email.lowercased()).setData([
             "name": name,
-            "email": email,
-            "photoURL": photoURL?.absoluteString
+            "email": email.lowercased(),
+            "expectedExpenses": user.expectedExpenses,
+            "budget": user.budget,
+            "spent": user.spent
         ], completion: {(error) in
             if error == nil {
                 self.navigationController?.popViewController(animated: true)
@@ -111,77 +129,77 @@ class EditProfileViewController: UIViewController {
         })
     }
     
-    func getMenuImagePicker() -> UIMenu{
-        var menuItems = [
-            UIAction(title: "Camera",handler: {(_) in
-                self.pickUsingCamera()
-            }),
-            UIAction(title: "Gallery",handler: {(_) in
-                self.pickPhotoFromGallery()
-            })
-        ]
-        return UIMenu(title: "Select source", children: menuItems)
-    }
-    
-    func pickUsingCamera() {
-        let cameraController = UIImagePickerController()
-        cameraController.sourceType = .camera
-        cameraController.allowsEditing = true
-        cameraController.delegate = self
-        present(cameraController, animated: true)
-    }
-    
-    func pickPhotoFromGallery() {
-        var configuration = PHPickerConfiguration()
-        configuration.filter = PHPickerFilter.any(of: [.images])
-        configuration.selectionLimit = 1
-                
-        let photoPicker = PHPickerViewController(configuration: configuration)
-                
-        photoPicker.delegate = self
-        present(photoPicker, animated: true, completion: nil)
-    }
+//    func getMenuImagePicker() -> UIMenu{
+//        var menuItems = [
+//            UIAction(title: "Camera",handler: {(_) in
+//                self.pickUsingCamera()
+//            }),
+//            UIAction(title: "Gallery",handler: {(_) in
+//                self.pickPhotoFromGallery()
+//            })
+//        ]
+//        return UIMenu(title: "Select source", children: menuItems)
+//    }
+//
+//    func pickUsingCamera() {
+//        let cameraController = UIImagePickerController()
+//        cameraController.sourceType = .camera
+//        cameraController.allowsEditing = true
+//        cameraController.delegate = self
+//        present(cameraController, animated: true)
+//    }
+//
+//    func pickPhotoFromGallery() {
+//        var configuration = PHPickerConfiguration()
+//        configuration.filter = PHPickerFilter.any(of: [.images])
+//        configuration.selectionLimit = 1
+//
+//        let photoPicker = PHPickerViewController(configuration: configuration)
+//
+//        photoPicker.delegate = self
+//        present(photoPicker, animated: true, completion: nil)
+//    }
 
 }
 
-extension EditProfileViewController:PHPickerViewControllerDelegate {
-    func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
-        dismiss(animated: true)
-        
-        print(results)
-        
-        let itemprovider = results.map(\.itemProvider)
-        
-        for item in itemprovider{
-            if item.canLoadObject(ofClass: UIImage.self){
-                item.loadObject(ofClass: UIImage.self, completionHandler: { (image, error) in
-                    DispatchQueue.main.async{
-                        if let uwImage = image as? UIImage{
-                            self.editProfileScreen.buttonTakePhoto.setImage(
-                                uwImage.withRenderingMode(.alwaysOriginal),
-                                for: .normal
-                            )
-                            self.profileImage = uwImage
-                        }
-                    }
-                })
-            }
-        }
-    }
-}
-
-extension EditProfileViewController: UINavigationControllerDelegate, UIImagePickerControllerDelegate{
-    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-        picker.dismiss(animated: true)
-        
-        if let image = info[.editedImage] as? UIImage{
-            self.editProfileScreen.buttonTakePhoto.setImage(
-                image.withRenderingMode(.alwaysOriginal),
-                for: .normal
-            )
-            self.profileImage = image
-        }else{
-            // Do your thing for No image loaded...
-        }
-    }
-}
+//extension EditProfileViewController:PHPickerViewControllerDelegate {
+//    func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
+//        dismiss(animated: true)
+//
+//        print(results)
+//
+//        let itemprovider = results.map(\.itemProvider)
+//
+//        for item in itemprovider{
+//            if item.canLoadObject(ofClass: UIImage.self){
+//                item.loadObject(ofClass: UIImage.self, completionHandler: { (image, error) in
+//                    DispatchQueue.main.async{
+//                        if let uwImage = image as? UIImage{
+//                            self.editProfileScreen.buttonTakePhoto.setImage(
+//                                uwImage.withRenderingMode(.alwaysOriginal),
+//                                for: .normal
+//                            )
+//                            self.profileImage = uwImage
+//                        }
+//                    }
+//                })
+//            }
+//        }
+//    }
+//}
+//
+//extension EditProfileViewController: UINavigationControllerDelegate, UIImagePickerControllerDelegate{
+//    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+//        picker.dismiss(animated: true)
+//
+//        if let image = info[.editedImage] as? UIImage{
+//            self.editProfileScreen.buttonTakePhoto.setImage(
+//                image.withRenderingMode(.alwaysOriginal),
+//                for: .normal
+//            )
+//            self.profileImage = image
+//        }else{
+//            // Do your thing for No image loaded...
+//        }
+//    }
+//}
