aaeee3bb56e87eac49969e44d0fb9a0cde6e5b2e
TylerSchaefer4 <93676493+TylerSchaefer4@users.noreply.github.com>
2023-06-25 20:05:17 -0400

    add transaction impl

--- BudgetBuddy/BudgetBuddy/AddTransaction/AddTransactionViewController.swift
+++ BudgetBuddy/BudgetBuddy/AddTransaction/AddTransactionViewController.swift
@@ -6,24 +6,30 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseAuth
 
 class AddTransactionViewController: UIViewController {
 
+    var addTransactionView = AddTransactionView()
+    var currentUser:FirebaseAuth.User!
+    
+    
+    override func loadView() {
+        view = addTransactionView
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        title = "Add Transaction"
+        
+        self.addTransactionView.addButton.addTarget(self, action: #selector(onAddButtonTapped), for: .touchUpInside)
+        //self.getUserInformation()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @objc func onAddButtonTapped(){
+        
     }
-    */
 
 }
